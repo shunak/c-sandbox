f4e9607e041b51ca1cfb58a5bf3db5cfaeef5ed5
shunak <shuna966@gmail.com>
2018-10-16 00:04:07 +0900

    181015

--- double_pointer.c
+++ double_pointer.c
@@ -4,24 +4,40 @@
 
 
 
+
+
+
+void twice(){
+
+
+
+    
+}
+
+
+
 void disp_rand(int a[]){
 
+    printf("発生した乱数:\n");
     srand((unsigned) time(NULL));
     int b;
     for(int i = 0; i < 5; i++)
     {
 
         b = rand() % 100 + 1;
-        a[i]=b; 
+        a[i]=b;
 
-        printf("%d ",a[i]);
+        printf("%d ", a[i]);
 
     }
+
     printf("\n");
-
-
-
 }
+
+
+
+
+
 
 void main(){
 
@@ -29,7 +45,6 @@ void main(){
     int a[5];
 
     disp_rand(a);
-
 
 
 }
