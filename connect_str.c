f4a2ee7ca8653658a1f59b2cc0133ab75556144d
shunak <shuna966@gmail.com>
2018-10-18 23:50:45 +0900

    181018

---
+++ connect_str.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+// #include 
+// #define SIZE 3
+
+int main(void){
+
+
+char abc1[3];
+char abc3[4];
+
+printf("文字列1:");
+scanf("%s",abc1);
+
+printf("文字列2:");
+scanf("%s",abc3);
+
+
+printf("%s",abc1);
+printf("\n");
+
+
+
+return 0;
+
+}
+
+
+
+
+
